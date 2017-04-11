f8a9d7a6435492f6a6a62f89b4d4347bc33381bf
Daniel Bugl <me@omnidan.net>
2017-04-11 12:25:39 +0200

    new website

---
+++ Readme.md
@@ -0,0 +1,48 @@
+# [touchlay.com](https://touchlay.com)
+
+## Updating content
+
+* German texts: [_includes/de](https://github.com/TouchLay/touchlay.com/tree/gh-pages/_includes/de)
+* English texts: [_includes/en](https://github.com/TouchLay/touchlay.com/tree/gh-pages/_includes/en)
+
+## Setup / Running
+
+ * Make sure you have Ruby gems installed.
+ * Get jekyll: `gem install jekyll`
+ * Get dependencies: `npm install -g bower-installer`, then run `bower-installer`
+
+Now you can run the website in development mode: `./dev.sh`
+
+
+## Structure
+
+The *layout* is located at `_layouts/default.html`, which contains the js and css
+imports as well as header and footer of the page.
+
+The *main content* of the page is located at `_includs/page.html`. The page
+includes content from the `de` and `en` folders where the translated content is
+located. If you want to add a markdown content block you can include it via:
+
+```
+{% capture stuff %}{% include {{page.lang}}/background.md %}{% endcapture %}
+{{ stuff | markdownify }}
+```
+
+Always make sure that the file exists for every language you want to generate.
+
+The different versions are then generated through the `language` attribute in
+`index.html`.
+
+For example, The *german* version is located in `de/index.html` with `language`
+set to `de`.
+
+If you want to add sub-pages, add them in the language folder (`en`, `de`, ...)
+and link to them with `{{page.lang}}/PAGE.html`.
+
+
+## Assets
+
+Always make sure to load assets with the absolute path.
+
+To load images with the absolute path just prepend `{{site.url}}`, which will
+resolve to `//touchlay.com` (or `//localhost:4000` in development mode).
---
+++ js/main.js
@@ -0,0 +1,197 @@
+---
+---
+
+document.body.className = document.body.className.replace("no-js","js") // set that we have js
+var lang = $('body').data('lang') // get current langauge
+
+// detect language and forwared to right language (only from /index.html)
+if (location.pathname == "/") {
+  var browserLang = window.navigator.userLanguage || window.navigator.language;
+  window.location.href = (browserLang === 'de') ? '/de/' : '/en/'
+}
+
+$(document).ready(function() {
+  // Submit the form with an ajax/jsonp request.
+  // Based on http://stackoverflow.com/a/15120409/215821
+  function submitSubscribeForm(data) {
+    $.ajax({
+      type: "GET",
+      url: '//touchlay.us6.list-manage.com/subscribe/post-json?u=61192115196961388ececdeb1&amp;id=7425f66543',
+      data: data,
+      cache: false,
+      dataType: "jsonp",
+      jsonp: "c", // trigger MailChimp to return a JSONP response
+      contentType: "application/json; charset=utf-8",
+
+      error: function(error){
+        // According to jquery docs, this is never called for cross-domain JSONP requests
+      },
+
+      success: function(data){
+        $('#mc-embedded-subscribe').removeClass('loading');
+        var resultMessage = data.msg || "Sorry. Unable to subscribe. Please try again later.";
+
+        var helper = $('.newsletter-response');
+        if (data.result != "success") {
+          if (resultMessage && resultMessage.indexOf("already subscribed") >= 0) {
+            helper.addClass('success')
+            $('#mc-embedded-subscribe-form').css('display', 'none')
+            $('.newsletter-description').css('display', 'none')
+            helper.html("You're already subscribed. Thank you.");
+          } else {
+            helper.addClass('error')
+            if (resultMessage.split('-').length >= 2) {
+              helper.html(resultMessage.split('-')[1]);
+            } else {
+              helper.html(resultMessage);
+            }
+          }
+        } else {
+          helper.addClass('success')
+          $('#mc-embedded-subscribe-form').css('display', 'none')
+          $('.newsletter-description').css('display', 'none')
+          helper.html('Thank you!\nYou must confirm the subscription in your inbox.');
+        }
+        helper.css('display', 'block');
+      }
+    });
+  }
+
+  $("#mc-embedded-subscribe-form").submit(function(e){
+    $('#mc-embedded-subscribe').addClass('loading');
+    e.preventDefault();
+    submitSubscribeForm($("#mc-embedded-subscribe-form").serialize());
+  });
+
+  $('#mc-embedded-subscribe').click(function() {
+    $("#mc-embedded-subscribe-form").submit();
+  });
+
+  // Handle cookie dialog
+  if ($.fn.cookie('cookie-banner-dismissed')) { $('.cookie-banner').hide(); }
+  else { $('.cookie-banner').show(); }
+
+  $('#accept-cookie').click(function() {
+    $.fn.cookie('cookie-banner-dismissed', true)
+    $('.cookie-banner').hide();
+  })
+
+
+  // Handle autocomplete
+  var autocompleteElements = $.map({{ site.data.base.interessts | jsonify }}[lang], function(item, index) {
+    return { id: index + 1, text: item };
+  })
+  var placeholder = {{ site.data.base.interesstsplaceholder | jsonify }}[lang]
+
+  // Handle preselected interessts
+  var preInteressts = $('body').data('interessts')
+  if (preInteressts) {
+    var preselect = $.map(preInteressts.split(','), function(item){
+      return autocompleteElements[item]
+    })
+  }
+
+  $('#contact-interested').selectivity({
+    items: autocompleteElements,
+    multiple: true,
+    placeholder: placeholder,
+    data: preInteressts && (preselect || [])
+  });
+
+  var countryPlaceholder = {{ site.data.base.countryplaceholder | jsonify }}[lang]
+
+  $('#contact-country').selectivity({
+    items: {{ site.data.countries | jsonify }}[lang],
+    multiple: false,
+    placeholder: countryPlaceholder
+  });
+
+  // Handle scooch carousel(s)
+  $('.m-scooch').scooch({
+    infinite: true
+  })
+
+  // Validate Contact form
+  var happy = {
+    email: function (val) {
+        return /^(?:\w+\.?\+?)*\w+@(?:\w+\.)+\w+$/.test(val);
+    },
+    maxLength: function (val, length) {
+        return val.length <= length;
+    },
+  };
+  $('#contact-form').isHappy({
+    fields: {
+      '#contact-name': {
+        required: true,
+      },
+      /*'#contact-country .selectivity-single-select-input': {
+        required: true,
+      },*/
+      '#contact-message': {
+        test: function(val) { return happy.maxLength(val, 5000) },
+        message: 'Message is too long!'
+      },
+      '#contact-email': {
+        required: true,
+        test: happy.email
+      }
+    },
+    /*unHappy: function (ev) {
+      ev.preventDefault();
+
+      // set "unhappy" class on auto-complete input
+      if ($('#contact-country .selectivity-single-select-input').hasClass('unhappy')) {
+        $('#contact-country .selectivity-single-select').addClass('unhappy')
+      }
+    },*/
+    happy: function(ev) {
+      ev.preventDefault();
+
+      // add to newsletter if checkbox is checked
+      if ($('#checkbox-newsletter').prop('checked')) {
+        submitSubscribeForm('EMAIL=' + encodeURIComponent($('#contact-email').val()));
+      }
+
+      sendInquiry({
+        name: $('#contact-name').val(),
+        email: $('#contact-email').val(),
+        message: $('#contact-message').val(),
+        country: $('#contact-country').selectivity('data') && $('#contact-country').selectivity('data').text,
+        interests: $('#contact-interested').selectivity('data') && $.map($('#contact-interested').selectivity('data'), function(item) { return item.text })
+      })
+    }
+  });
+
+  // Handle contact form request
+  function sendInquiry (data) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://inquirer.exp.touchlay.com/', true);
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4 && xhr.status == 204) {
+        console.log('success');
+        $('#contact-checkbox').hide();
+        $('#contact-checkbox-description').hide();
+        $('#contact-submit').hide();
+        $('#contact-confirmation-success').show();
+      }
+      if (xhr.status != 204) {
+        console.log('error, status code:', xhr.status);
+        $('#contact-checkbox').hide();
+        $('#contact-checkbox-description').hide();
+        $('#contact-submit').hide();
+        $('#contact-confirmation-error').show();
+      }
+    }
+    xhr.send(JSON.stringify(data));
+  }
+
+  // handle cta highlight
+  $('.cta-highlight').click(function() {
+    $('#contact-form').addClass('highlight')
+    setTimeout(function() {
+      $('#contact-form').removeClass('highlight')
+    }, 1000)
+  })
+});
